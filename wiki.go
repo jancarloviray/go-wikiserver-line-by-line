6ba141d1e2a87d0306638787793c48730446cccf
Jan Carlo Viray <jc.viray@bluecava.com>
2014-09-06 02:23:47 -0700

    error handling and save functionality working

--- wiki.go
+++ wiki.go
@@ -37,7 +37,7 @@ func main() {
 	//	are matched.
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	//http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -101,7 +101,12 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	//	nil; otherwise it is t. There must be at least one file.
 
 	// Note that this returns a *template.Template
-	t, _ := template.ParseFiles(tmpl + ".html")
+	t, err := template.ParseFiles(tmpl + ".html")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	// func (t *Template) Execute(wr io.Writer, data interface{}) error
 	//	Execute applies a parsed template to the specified data object, writing
@@ -109,7 +114,12 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	//	its output, execution stops, but partial results may already have been
 	//	written to the output writer. A template may be executed safely in
 	//	parallel.
-	t.Execute(w, p)
+	err = t.Execute(w, p)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 // http://golang.org/pkg/net/http/#ResponseWriter
@@ -131,21 +141,26 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 
 	// If page does not exist, redirect to an edit page to create content
-	if err != {
+	if err != nil {
 		// http.StatusFound = 302 & adds Location header to HTTP Response
-		http.Redirect(w, r, "/edit" + title, http.StatusFound)
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
 
 	renderTemplate(w, "view", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request){
+func saveHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/save/"):]
 	body := r.FormValue("body")
+	fmt.Println(body)
 	p := &Page{Title: title, Body: []byte(body)}
-	p.save()
-	http.Redirect(w, r, "/view/" + title, http.StatusFound)
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
